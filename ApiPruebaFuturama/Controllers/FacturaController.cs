bd39336e5914278ba7f875c0bb48ca5e0db4fb68
Ruben Cardoza <rubencardoza611@gmail.com>
2023-02-05 14:54:05 -0700

    Add project files.

---
+++ ApiPruebaFuturama/Controllers/FacturaController.cs
@@ -0,0 +1,33 @@
+﻿using Facturama;
+using Facturama.Models.Request;
+using Facturama.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPruebaFuturama.Controllers
+{
+	[ApiController]
+	[Route("cdfi")]
+	public class FacturaController : ControllerBase
+	{
+		[HttpGet]
+		[Route("listar")]
+		public dynamic listarCDFIS()
+		{
+			var facturama = new FacturamaApiMultiemisor("pruebas", "pruebas2011");
+			
+			var cdfis = facturama.Cfdis.List();
+			
+			return cdfis;
+		}
+
+		[HttpGet]
+		[Route("listarPorId")]
+		public dynamic listarCdfiPorId(string id) {
+			var facturama = new FacturamaApiMultiemisor("pruebas", "pruebas2011");
+
+			var Cdfi = facturama.Cfdis.Retrieve(id);
+			
+			return Cdfi;
+		}
+	}
+}
